b63e4413e7ac65e32f88ff2fe20f821d52dc151b
Chris Tsang <ctsang@ripple.com>
2021-11-03 14:29:25 -0700

    add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import logo from "./logo.svg";
 import { xrplClient } from "./XrplApiSandbox";
 import {
   createConditionalEscrow,
@@ -70,40 +69,29 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <div className="App-logs">
-          {logs.map((log) => {
-            if (typeof log === "string") {
-              return (
-                <p key={Math.random()} className="App-console-log">
-                  {log}
-                </p>
-              );
-            } else if (typeof log === "object") {
-              return (
-                <div key={Math.random()}>
-                  <pre>{JSON.stringify(log, null, 2)}</pre>
-                </div>
-              );
-            }
-            return null;
-          })}
-        </div>
-
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        src={process.env.PUBLIC_URL + "/piggy_bank.jpg"}
+        className="App-logo"
+        alt="logo"
+      />
+      <div className="App-logs">
+        {logs.map((log) => {
+          if (typeof log === "string") {
+            return (
+              <p key={Math.random()} className="App-console-log">
+                {log}
+              </p>
+            );
+          } else if (typeof log === "object") {
+            return (
+              <div key={Math.random()}>
+                <pre>{JSON.stringify(log, null, 2)}</pre>
+              </div>
+            );
+          }
+          return null;
+        })}
+      </div>
     </div>
   );
 }
